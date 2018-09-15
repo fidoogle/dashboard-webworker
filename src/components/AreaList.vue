aae71c76ce0a78ca0b9939ff8ae4191b91685b6c
Fidel Guajardo <fidelg3@gmail.com>
2018-09-15 14:02:39 -0500

    Begin change select list to custom drop down

--- src/components/AreaList.vue
+++ src/components/AreaList.vue
@@ -1,8 +1,18 @@
 <template>
   <div>
-      <select style="display:inline; width:360px" @change="updateArea(selected)" v-model="selected">
-          <option v-for="(value, key) in getAreas" :key="key" :value="value.ID" :selected="selected==value.ID">{{value.Title}}</option>
-      </select>
+    <select style="display:inline; width:360px" @change="updateArea(selected)" v-model="selected">
+      <option v-for="(value, key) in getAreas" :key="key" :value="value.ID" :selected="selected==value.ID">{{value.Title}}</option>
+    </select>
+    <div class="area-select">
+      <div class="area-select-row">
+        <div class="area-title"><b>Area</b></div>
+        <div class="area-complete"><b>{{stageTitle}}</b></div>
+      </div>
+      <div class="area-select-row" v-for="(value, key) in getAreas" :key="key" :value="value.ID">
+        <div class="area-title">{{value.Title}}</div>
+        <div class="area-complete">{{percent(value.ID)}}</div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -18,11 +28,32 @@ export default {
     },
 
     computed: {
-        ...Vuex.mapGetters(["getAreas", "getSelectedArea", "getSelectedStage"])
+        ...Vuex.mapGetters(["getAreas", "getSelectedArea", "getSelectedStage", "getStages", "getStagePercents"]),
+
+        stagePercents() {
+          const percents = this.getStagePercents.filter((item) => {
+              return (item.stageID == this.getSelectedStage); //don't use ===
+           });
+          return percents;
+         },
+
+        stageTitle() {
+          const stage = this.getStages.filter((item) => {
+              return (item.ID == this.getSelectedStage); //don't use ===
+           });
+          return stage[0].Title;
+        }
     },
 
     methods: {
         ...Vuex.mapMutations(["setFromRoute", "setLoading", "setSelectedArea"]),
+
+        percent(id) {
+          const areaPercent = this.stagePercents.filter((item) => {
+              return (item.areaID == id); //don't use ===
+           });
+          return areaPercent[0].percent;
+         },
 
         setDefaultArea(area) {
             let areaID = area;
@@ -64,3 +95,38 @@ export default {
     }
 }
 </script>
+<style>
+.area-select {
+  z-index: 10;
+  width: 360px;
+  border: 1px solid #ccc;
+  border-radius: 6px;
+  padding: 10px;
+  background-color: white;
+  -webkit-box-shadow: -1px 10px 39px 0px rgba(0,0,0,0.75);
+  -moz-box-shadow: -1px 10px 39px 0px rgba(0,0,0,0.75);
+  box-shadow: -1px 10px 39px 0px rgba(0,0,0,0.75);
+
+  display: flex;
+  flex-direction: column;
+  flex-wrap: nowrap;
+  justify-content: flex-start;
+  align-items: flex-start;
+}
+.area-select-row {
+  flex: 0 1 auto;
+  align-self: stretch;
+
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  justify-content: space-between;
+  align-items: center;
+}
+.area-title {
+  color: black;
+}
+.area-complete {
+  color: black;
+}
+</style>
